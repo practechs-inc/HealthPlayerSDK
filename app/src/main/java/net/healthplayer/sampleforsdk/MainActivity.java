6a335d9977c0bbb7c0328e8d96d5e6b33ac3fdf2
kkiyama117 <k.kiyama117@gmail.com>
2019-04-24 14:05:04 +0900

    Change view

--- app/src/main/java/net/healthplayer/sampleforsdk/MainActivity.java
+++ app/src/main/java/net/healthplayer/sampleforsdk/MainActivity.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements SensorObserver, DeviceObserver {
 //    public class MainActivity extends AppCompatActivity implements SensorObserver, DeviceObserver {
 
-    HealthPlayerDeviceManager dm = HealthPlayerDeviceManager.getInstance();
+    private HealthPlayerDeviceManager dm = HealthPlayerDeviceManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
