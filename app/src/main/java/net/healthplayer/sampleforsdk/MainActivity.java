c2bc23a3696a132e82ed273d853808ad3d2e863f
kkiyama117 <k.kiyama117@gmail.com>
2019-04-25 10:29:43 +0900

    Update file

--- app/src/main/java/net/healthplayer/sampleforsdk/MainActivity.java
+++ app/src/main/java/net/healthplayer/sampleforsdk/MainActivity.java
@@ -1,25 +1,34 @@
 package net.healthplayer.sampleforsdk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import net.healthplayer.sdk.*;
 import net.healthplayer.sdk.util.LogUtil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 
 
+/**
+ * Sample application for using device
+ */
 public class MainActivity extends AppCompatActivity implements DeviceObserver {
-//    public class MainActivity extends AppCompatActivity implements SensorObserver, DeviceObserver {
 
     private HealthPlayerDeviceManager dm = HealthPlayerDeviceManager.getInstance();
     private HealthPlayerModelManager mm = HealthPlayerModelManager.getInstance();
     private TextView mainTextView;
+    private ArrayList<HealthcareDataEntity> healthcareDataEntities;
+    // このActivityに接続しているデバイスの名前一覧
+    private ArrayList<String> attachedDeviceNames;
 
+    /**
+     * onCreate function
+     * <p>
+     * Please initialize `HealthPlayerModelManager` and `HealthPlayerDeviceManager`
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // prepare variables
@@ -28,10 +37,12 @@ public class MainActivity extends AppCompatActivity implements DeviceObserver {
         // normal initialize app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        healthcareDataEntities = new ArrayList<>();
+        attachedDeviceNames =new ArrayList<>();
         findViewById(R.id.button).setOnClickListener(
-                this::buttonClick
+                this::getDataFromServer
         );
-        mainTextView=findViewById(R.id.mainTextView);
+        mainTextView = findViewById(R.id.mainTextView);
 
         // initialize SDK
         mainTextView.setText(getString(R.string.initializing));
@@ -53,40 +64,73 @@ public class MainActivity extends AppCompatActivity implements DeviceObserver {
             e.printStackTrace();
         }
 
+        // UserのProfileをサーバーに登録する
         try {
             // get profiles
-            List<UserProfileEntity> profiles = new ArrayList<UserProfileEntity>();
+            List<UserProfileEntity> profiles = new ArrayList<>();
             mm.getProfile(profiles);
-            // Create user entity
-            Calendar birth = Calendar.getInstance();
-            birth.set(1998, 1, 17);
-            String gender = "1";
-            double height = 160.0;
-            double weight = 50.2;
-            String area = "tokyo";
-            profile = new UserProfileEntity(birth, gender, height, weight, area);
 
             // Set User profile of this user
+            profile = createTestProfile();
             mm.setProfile(profile);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        // デバイスからのEventの通知先(Activity等)を設定します
         dm.attachDeviceObserver(this);
+        mainTextView.setText(getString(R.string.completeinit));
     }
 
-    // method for click event
-    private void buttonClick(View v) {
+    /**
+     * プロファイルを作っています
+     *
+     * @return UserProfileEntity
+     */
+    private UserProfileEntity createTestProfile() {
+        // Create user entity
+        Calendar birth = Calendar.getInstance();
+        birth.set(1998, 1, 17);
+        String gender = "1";
+        double height = 160.0;
+        double weight = 50.2;
+        String area = "tokyo";
+        return new UserProfileEntity(birth, gender, height, weight, area);
+    }
+
+    /**
+     * 今回はボタンをクリック時に, サーバーのデータを更新してからデータを表示します
+     *
+     * @param v view
+     */
+    private void getDataFromServer(View v) {
+        this.connectHeartRateDevice();
+        // データを更新します
+        mm.synchronize();
+
+        // ある期間の血圧データをサーバーから取得してみます.
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+        start.set(2019, 1, 1);
+        end.set(2019, 5, 1);
+        mm.acquireHealthcareData("heartRate", start, end, healthcareDataEntities);
+        mainTextView.setText(getString(R.string.data_from_device, "heartRate"
+                , healthcareDataEntities));
+    }
+
+    /**
+     * 心拍計を接続します
+     */
+    private void connectHeartRateDevice() {
+        // Bluetooth で接続します
         String deviceName = "UA-851PBT-C";
+        attachedDeviceNames.add(deviceName);
         dm.invokeBluetooth(deviceName);
-//        mm.acquireHealthcareData();
-        dm.revokeBluetooth(deviceName);
     }
 
     @Override
     protected void onDestroy() {
-        // deprecated
-        // こちら使用できますか
-        dm.revokePassometer();
+        // 接続している可能性のある全てのデバイスとのBluetooth接続を切ります
+        attachedDeviceNames.forEach(deviceName-> dm.revokeBluetooth(deviceName));
         super.onDestroy();
     }
 
@@ -96,17 +140,32 @@ public class MainActivity extends AppCompatActivity implements DeviceObserver {
         // do nothing
     }
 
+    /**
+     * デバイスからの通知を受け取った時の処理
+     * <p>
+     * 今回はサーバーにデータを保存する処理のみ行っています
+     *
+     * @param deviceHandler        DeviceHandler
+     * @param healthcareDataEntity healthcare デバイスの一つの測定結果
+     */
     @Override
     public void notify(DeviceHandler deviceHandler, HealthcareDataEntity healthcareDataEntity) {
-        mainTextView.setText(getString(R.string.data_from_device,
-                deviceHandler.getDeviceName(), healthcareDataEntity.getValues()));
+        mm.storeHealthcareData(healthcareDataEntity);
     }
 
+
+    /**
+     * デバイスからの通知を受け取った時の処理
+     * <p>
+     * 今回はサーバーにデータを保存する処理のみ行っています
+     *
+     * @param deviceHandler        DeviceHandler
+     * @param healthcareDataEntities healthcare デバイスの測定結果
+     */
     @Override
     public void notify(final DeviceHandler deviceHandler, HealthcareDataEntity[] healthcareDataEntities) {
         for (HealthcareDataEntity healthcareDataEntity : healthcareDataEntities) {
             this.notify(deviceHandler, healthcareDataEntity);
         }
-        this.notify();
     }
 }
